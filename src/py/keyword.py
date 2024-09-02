f2f5c296c5d087dfdd5522a67b07c25f36565ba6
Seokxkyu <kyuseok000@gmail.com>
2024-09-02 13:43:27 +0900

    add keyword_beta

--- src/py/keyword.py
+++ src/py/keyword.py
@@ -1,31 +1,58 @@
+import os
 import pandas as pd
-from keybert import KeyBERT
-from sentence_transformers import SentenceTransformer
+import http.client
+import json
 
-class KeywordExtractor:
-    def __init__(self, model_name='paraphrase-multilingual-MiniLM-L12-v2'):
-        model = SentenceTransformer(model_name)
-        self.kw_model = KeyBERT(model=model)
+class CompletionExecutor:
+    def __init__(self):
+        self._host = 'clovastudio.apigw.ntruss.com'
+        self._api_key = os.getenv('CLOVA_API_KEY')
+        self._api_key_primary_val = os.getenv('CLOVA_API_PRIMARY_KEY')
+        self._request_id = os.getenv('CLOVA_REQUEST_ID')
+        self._seg_min_size = 500  # 최소 요약 단위 크기
+        self._seg_max_size = 2000  # 최대 요약 단위 크기
+        self._seg_count = -1  # 모델이 최적 문단 수로 분리하도록 설정
+        self._auto_sentence_splitter = True  # 문장 분리 허용 여부
+        self._include_ai_filters = True  # AI 필터 적용 여부
 
-    def extract_keywords(self, text, keyphrase_ngram_range=(1, 1), use_mmr=True, diversity=0.5, use_maxsum=False, nr_candidates=20):
-        keywords = self.kw_model.extract_keywords(
-            text,
-            keyphrase_ngram_range=keyphrase_ngram_range,
-            use_mmr=use_mmr,
-            diversity=diversity,
-            use_maxsum=use_maxsum,
-            nr_candidates=nr_candidates
-        )
-        return {kw[0]: kw[1] for kw in keywords}
+    def _send_request(self, completion_request):
+        headers = {
+            'Content-Type': 'application/json; charset=utf-8',
+            'X-NCP-CLOVASTUDIO-API-KEY': self._api_key,
+            'X-NCP-APIGW-API-KEY': self._api_key_primary_val,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id
+        }
 
-    def extract(self, input_parquet, output_parquet):
-        df = pd.read_parquet(input_parquet)
-        df['keywords'] = df['content'].apply(lambda x: self.extract_keywords(x))
-        df.to_parquet(output_parquet, index=False)
-        return df
+        conn = http.client.HTTPSConnection(self._host)
+        conn.request('POST', '/testapp/v1/api-tools/keyword-extraction/v1', json.dumps(completion_request), headers)
+        response = conn.getresponse()
+        result = json.loads(response.read().decode(encoding='utf-8'))
+        conn.close()
+        return result
 
-def extract_keywords(ds_nodash, output_dir, **kwargs):
-    input_parquet = f"{output_dir}/summarize/{ds_nodash}.parquet"
+    def execute(self, text):
+        request_data = {
+            "texts": [text],
+            "segMinSize": self._seg_min_size,
+            "segMaxSize": self._seg_max_size,
+            "segCount": self._seg_count,
+            "autoSentenceSplitter": self._auto_sentence_splitter,
+            "includeAiFilters": self._include_ai_filters
+        }
+        res = self._send_request(request_data)
+        if res['status']['code'] == '20000':
+            return res['result']['keywords']
+        else:
+            return 'Error: ' + res['status']['message']
+
+def extract_keywords(ds_nodash, output_dir):
+    input_parquet = f"{output_dir}/content/{ds_nodash}.parquet"
     output_parquet = f"{output_dir}/keywords/{ds_nodash}.parquet"
-    extractor = KeywordExtractor()
-    extractor.extract(input_parquet, output_parquet)
+    
+    df = pd.read_parquet(input_parquet)
+    completion_executor = CompletionExecutor()
+
+    df['keywords'] = df['content'].apply(lambda x: completion_executor.execute(x))
+    df.to_parquet(output_parquet, index=False)
+
+    return df
---
+++ src/py/keyword_beta.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from keybert import KeyBERT
+from sentence_transformers import SentenceTransformer
+
+class KeywordExtractor:
+    def __init__(self, model_name='paraphrase-multilingual-MiniLM-L12-v2'):
+        model = SentenceTransformer(model_name)
+        self.kw_model = KeyBERT(model=model)
+
+    def extract_keywords(self, text, keyphrase_ngram_range=(1, 1), use_mmr=True, diversity=0.5, use_maxsum=False, nr_candidates=20):
+        keywords = self.kw_model.extract_keywords(
+            text,
+            keyphrase_ngram_range=keyphrase_ngram_range,
+            use_mmr=use_mmr,
+            diversity=diversity,
+            use_maxsum=use_maxsum,
+            nr_candidates=nr_candidates
+        )
+        return {kw[0]: kw[1] for kw in keywords}
+
+    def extract(self, input_parquet, output_parquet):
+        df = pd.read_parquet(input_parquet)
+        df['keywords'] = df['content'].apply(lambda x: self.extract_keywords(x))
+        df.to_parquet(output_parquet, index=False)
+        return df
+
+def extract_keywords(ds_nodash, output_dir, **kwargs):
+    input_parquet = f"{output_dir}/summarize/{ds_nodash}.parquet"
+    output_parquet = f"{output_dir}/keywords/{ds_nodash}.parquet"
+    extractor = KeywordExtractor()
+    extractor.extract(input_parquet, output_parquet)
